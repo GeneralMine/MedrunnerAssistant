dd7db72941a1791bf0aa1a0919d4514a65478b6b
Lübbi <luebbi5000@gmail.com>
2025-07-13 07:39:39 +0200

    More fiddling with teamUpdate

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ console.log("Socket is " + ws.state);
 
 if (process.env.ENABLE_ALERTS === "true") {
 	ws.on("EmergencyCreate", async (alert) => {
-//		console.log("New alert received: " + JSON.stringify(alert));
+		console.log("New alert received: " + JSON.stringify(alert));
 
 		try {
 			await playAudio(process.env.ALERT_SOUND);
@@ -38,7 +38,7 @@ if (process.env.ENABLE_ALERTS === "true") {
 if (process.env.ENABLE_CHATMESSAGE === "true") {
 	ws.on("ChatMessageCreate", async (chatMessage) => {
 	
-//		console.log("New chat message received: " + JSON.stringify(chatMessage));
+		console.log("New chat message received: " + JSON.stringify(chatMessage));
 		
 		if(process.env.USERID != chatMessage.senderId){
 			try {
@@ -54,9 +54,9 @@ if (process.env.ENABLE_CHATMESSAGE === "true") {
 if (process.env.ENABLE_TEAMUPDATE === "true") {
 	ws.on("TeamUpdate", async (teamUpdate) => {
 	
-//		console.log("New team update received: " + JSON.stringify(teamUpdate));
+		console.log("New team update received: " + JSON.stringify(teamUpdate));
 		
-		if(teamUpdate.waitList && teamUpdate.waitList.length > 0){
+//		if(teamUpdate.waitList && teamUpdate.waitList.length > 0){
 			teamUpdate.waitList.forEach(element => {
 				console.log(element.rsiHandle + " has requested to join the team");
 				console.log("After accepting this request, the team will be " + (teamUpdate.members.length+1) + " people big")
@@ -67,7 +67,7 @@ if (process.env.ENABLE_TEAMUPDATE === "true") {
 			} catch (e) {
 				console.error("Failed to play audio:", e);
 			}
-		}
+//		}
 	});
 }
 
