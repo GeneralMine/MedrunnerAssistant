3c4efde47169f32ced3f506cde8633d38d713ce8
Lübbi <luebbi5000@gmail.com>
2025-07-15 19:23:36 +0200

    modified main.js for team join requests

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const apiConfig = {
 
 const api = MedrunnerApiClient.buildClient(apiConfig);
 
-const self = await api.client.get();
+let self = await api.client.get();
 
 console.log("Authenticated as " + self.data.rsiHandle);
 
@@ -24,7 +24,7 @@ console.log("Socket is " + ws.state);
 
 if (process.env.ENABLE_ALERTS === "true") {
 	ws.on("EmergencyCreate", async (alert) => {
-		console.log("New alert received: " + JSON.stringify(alert));
+		console.log("New alert received: " + alert.missionName);
 
 		try {
 			await playAudio(process.env.ALERT_SOUND);
@@ -38,8 +38,7 @@ if (process.env.ENABLE_ALERTS === "true") {
 if (process.env.ENABLE_CHATMESSAGE === "true") {
 	ws.on("ChatMessageCreate", async (chatMessage) => {
 	
-		console.log("New chat message received: " + JSON.stringify(chatMessage));
-		
+		console.log("New chat message received: " + JSON.stringify(chatMessage.contents));
 		if(process.env.USERID != chatMessage.senderId){
 			try {
 				await playAudio(process.env.CHATMESSAGE_SOUND);
@@ -54,18 +53,20 @@ if (process.env.ENABLE_CHATMESSAGE === "true") {
 if (process.env.ENABLE_TEAMUPDATE === "true") {
 	ws.on("TeamUpdate", async (teamUpdate) => {
 	
-		console.log("New team update received: " + JSON.stringify(teamUpdate));
-		
-		if(teamUpdate.waitList && teamUpdate.waitList.length > 0){
-			teamUpdate.waitList.forEach(element => {
-				console.log(element.rsiHandle + " has requested to join the team");
-				console.log("After accepting this request, the team will be " + (teamUpdate.members.length+1) + " people big")
-			});
-			try {
-				await playAudio(process.env.TEAMUPDATE_SOUND);
-				console.log("Playback finished for teamupdate.");
-			} catch (e) {
-				console.error("Failed to play audio:", e);
+		console.log("New team update received");
+		await updateUser();
+		if(self.data.activeClass == 4){
+			if(teamUpdate.waitList && teamUpdate.waitList.length > 0){
+				teamUpdate.waitList.forEach(element => {
+					console.log(element.rsiHandle + " has requested to join the team");
+					console.log("After accepting this request, the team will be " + (teamUpdate.members.length+teamUpdate.waitList.length) + " people big")
+				});
+				try {
+					await playAudio(process.env.TEAMUPDATE_SOUND);
+					console.log("Playback finished for teamupdate.");
+				} catch (e) {
+					console.error("Failed to play audio:", e);
+				}
 			}
 		}
 	});
@@ -78,3 +79,7 @@ ws.onreconnected(async () => {
 ws.onclose(async () => {
 	console.log("Connection has been lost");
 });
+
+async function updateUser(){
+	self = await api.client.get();
+}
