e8aac89d4b39a2d4c09bb9f4b59555e6623515d5
Lübbi <75573579+luebbi5000@users.noreply.github.com>
2025-07-30 05:15:51 +0200

    Added showing of join order upon teamupdate (now in the correct branch) (#19)
    Co-authored-by: Marvin Raiser <marvin@raiser.dev>

--- main.js
+++ main.js
@@ -56,6 +56,13 @@ if (process.env.ENABLE_TEAMUPDATE === "true") {
 
 		if (self.data.activeClass == 4 && self.data.activeTeam == teamUpdate.id) {
 
+			console.log("Current join order for your team is:");
+			let i = 1;
+			teamUpdate.members.forEach((element) => {
+				console.log(i + ". " + element.rsiHandle);
+				i++;
+			});
+
 			if (teamUpdate.waitList && teamUpdate.waitList.length > 0) {
 				teamUpdate.waitList.forEach((element) => {
 					console.log(element.rsiHandle + " has requested to join the team");
